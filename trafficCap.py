ab965b0604cb08a241c6e4839a18bb4b744ddeda
TheWinrarz <danielaustinlee@gmail.com>
2018-10-31 10:04:31 -0400

    Create beacon Class

--- trafficCap.py
+++ trafficCap.py
@@ -3,6 +3,7 @@ import sys
 
 num = int(sys.argv[1])
 print("number specified " +  str(num))
+#only captures TCP packets (probably not safe assumption)
 packets = sniff(count=num, prn = lambda x: x.summary(), timeout=10, filter="tcp")
 print("packets sniffed")
 wrpcap("sampleTraffic.pcap", packets)
--- trafficGen.py
+++ trafficGen.py
@@ -1,7 +1,12 @@
 from scapy.all import *
+from beaconClass import Beacon
 
-#define beacon form
+#define network, class, network ip, number of hosts...
 
+#define beacon form (done in Beacon class)
+
+beacon1 = Beacon("10.20.30.40", "192.168.1.77", 300, 5)
+print(beacon1.signal().show())
 #generate N beacon signals
 
 #inject into captured traffic
