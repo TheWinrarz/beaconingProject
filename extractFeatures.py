1b89fb17de63b63a792842db6fc64a97418d6c0f
TheWinrarz <danielaustinlee@gmail.com>
2018-11-05 21:39:33 -0500

    update feature extraction

--- extractFeatures.py
+++ extractFeatures.py
@@ -2,6 +2,8 @@
 
 from scapy.all import *
 import sys
+import feather
+
 
 pcap_filename = sys.argv[1]
 label_filename = sys.argv[2]
@@ -14,10 +16,11 @@ f = open(label_filename, "r")
 for i in f.readlines():
 	labels.append(i)
 
-for num, packet in enumerate(packets):
-	packet_dict["packet" + str(num)] = (packet[IP].src, packet[IP].dst, packet.time, labels[num])
+for num, packet in enumerate(pcap):
+	packet_dict["packet" + str(num)] = (packet[IP].src, packet[IP].dst, packet.time, labels[num].rstrip())
 
 
+feather.write_dataframe(packet_dict, 'test.feather')
 
 #Cleanup
 f.close()
